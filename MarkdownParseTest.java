3bc1d4b7aba575c7c469267f5fa49140163ab48a
Joe Politz <joe.politz@gmail.com>
2022-01-31 15:35:52 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class MarkdownParseTest {
     @Test
     public void addition() {
-        assertEquals(2, 1 + 1);
+        assertEquals(99999999, 1 + 1);
     }
 
     @Test
