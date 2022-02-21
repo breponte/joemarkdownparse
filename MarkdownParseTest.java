721910f20c76de8f7b2146d0b67ad93df38cb081
Joe Politz <joe.politz@gmail.com>
2022-02-21 10:55:02 -0800

    .

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,11 +8,6 @@ import java.util.List;
 
 public class MarkdownParseTest {
     @Test
-    public void addition() {
-        assertEquals(99999999, 1 + 1);
-    }
-
-    @Test
     public void testFile1() throws IOException {
         String contents= Files.readString(Path.of("./test-file.md"));
         List<String> expect = List.of("https://something.com", "some-page.html");
